db425845b3efa2bdfe367d518a7d6aac09c58881
Dhanushri28 <dhanuachar121@gmail.com>
2025-12-15 10:28:06 +0530

    Fix LangChain imports and Streamlit Cloud error

--- app/vectorstore_utils.py
+++ app/vectorstore_utils.py
@@ -1,5 +1,5 @@
 from langchain_community.vectorstores import FAISS
-from langchain.embeddings import HuggingFaceEmbeddings
+from langchain_community.embeddings import HuggingFaceEmbeddings
 from typing import List
 
 def create_faiss_index(texts: List[str]) :
